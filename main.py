048725c89943f49891f76152273c31adf00538fa
Ainur935 <ainur935@mail.ru>
2021-09-02 20:04:52 +0300

    'First_class'

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
+class Dot: #класс для координат точек на поле
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
